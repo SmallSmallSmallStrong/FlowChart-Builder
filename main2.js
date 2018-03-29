9f0b0d3e0d0fff91b731d2bf4d3861746d23f276
shizhen <>
2018-03-29 16:40:31 +0800

    demo 创建

---
+++ main2.js
@@ -0,0 +1,136 @@
+$(function () {
+    jsPlumb.ready(function () {
+
+        //初始化JsPlumb实现创建对象应该可以创建多个
+        var color = "#E8C870";
+        var instance = jsPlumb.getInstance({
+            // notice the 'curviness' argument to this Bezier curve.  the curves on this page are far smoother
+            // than the curves on the first demo, which use the default curviness value.
+            Connector: ["Bezier", {curviness: 50}],
+            DragOptions: {cursor: "pointer", zIndex: 2000},
+            PaintStyle: {strokeStyle: color, lineWidth: 2},
+            EndpointStyle: {radius: 5, fillStyle: color},
+            HoverPaintStyle: {strokeStyle: "#7073EB"},
+            EndpointHoverStyle: {fillStyle: "#7073EB"},
+            Container: "flow-panel"
+        });
+
+
+        $('#flow-panel').on("drop",function (ev) {
+            //避免jsPlumb的事件冲突
+            if (ev.target.className.indexOf("_jsPlumb") >= 0) {
+                return;
+            }
+            //阻止默认事件
+            ev.preventDefault();
+            var mx = '' + ev.originalEvent.offsetX + 'px';
+            var my = '' + ev.originalEvent.offsetY + 'px';
+
+            console.log('on drop : ' + ev.originalEvent.dataTransfer.getData('text'));
+            //
+            var uid = new Date().getTime();
+            //添加节点 node
+            var node = addNode('flow-panel', 'node' + uid, 'node', {x: mx, y: my});
+            //添加一个出口连线
+            addPorts(instance, node, ['out'], 'output');
+            //添加2个进口的节点
+            addPorts(instance, node, ['in1', 'in2'], 'input');
+            instance.draggable($(node));
+        });
+        $('#flow-panel').ondragover(function (ev) {
+            //
+            ev.preventDefault();
+        })
+    });
+
+    /**
+     *  在参数1的后面追加一个div，id=参数2，内容是参数3，位置是参数4
+     * @param parentId 父node的id属性
+     * @param nodeId
+     * @param nodeLable
+     * @param position  位置
+     * @returns {*}
+     */
+    function addNode(parentId, nodeId, nodeLable, position) {
+        //调用d3.min.js的对象的select方法
+        var panel = d3.select("#" + parentId);
+        panel.append('div').style('width', '120px').style('height', '50px')
+            .style('position', 'absolute')
+            .style('top', position.y).style('left', position.x)
+            .style('border', '2px #9DFFCA solid').attr('align', 'center')
+            .attr('id', nodeId).classed('node', true)
+            .text(nodeLable);
+
+        return jsPlumb.getSelector('#' + nodeId)[0];
+    }
+});
+
+function addPorts(instance, node, ports, type) {
+    //Assume horizental layout
+    var number_of_ports = ports.length;
+    var i = 0;
+    var height = $(node).height();  //Note, jquery does not include border for height
+    var y_offset = 1 / (number_of_ports + 1);
+    var y = 0;
+
+    for (; i < number_of_ports; i++) {
+        var anchor = [0, 0, 0, 0];
+        var paintStyle = {radius: 5, fillStyle: '#FF8891'};
+        var isSource = false, isTarget = false;
+        if (type === 'output') {
+            anchor[0] = 1;
+            paintStyle.fillStyle = '#D4FFD6';
+            isSource = true;
+        } else {
+            isTarget = true;
+        }
+
+        anchor[1] = y + y_offset;
+        y = anchor[1];
+
+        instance.addEndpoint(node, {
+            uuid: node.getAttribute("id") + "-" + ports[i],
+            paintStyle: paintStyle,
+            anchor: anchor,
+            maxConnections: -1,
+            isSource: isSource,
+            isTarget: isTarget
+        });
+    }
+}
+
+
+function connectPorts(instance, node1, port1, node2, port2) {
+    // declare some common values:
+    var color = "gray";
+    var arrowCommon = {foldback: 0.8, fillStyle: color, width: 5},
+        // use three-arg spec to create two different arrows with the common values:
+        overlays = [
+            ["Arrow", {location: 0.8}, arrowCommon],
+            ["Arrow", {location: 0.2, direction: -1}, arrowCommon]
+        ];
+
+    var uuid_source = node1.getAttribute("id") + "-" + port1;
+    var uuid_target = node2.getAttribute("id") + "-" + port2;
+
+    instance.connect({uuids: [uuid_source, uuid_target]});
+}
+
+
+function getTreeData() {
+    var tree = [
+        {
+            text: "Nodes",
+            nodes: [
+                {
+                    text: "Node1",
+                },
+                {
+                    text: "Node2"
+                }
+            ]
+        }
+    ];
+
+    return tree;
+}
